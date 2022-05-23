14267beedb6877277c0aab2741ab8d3563bbdcad
anonymous <>
2022-05-23 22:33:04 +0800

    first commit

---
+++ query_strategies/__init__.py
@@ -0,0 +1,7 @@
+from .random_sampling import RandomSampling
+from .least_confidence import LeastConfidence
+from .margin_sampling import MarginSampling
+from .entropy_sampling import EntropySampling
+from .core_set import CoreSet
+from .badge_sampling  import BadgeSampling
+from .lhd_sampling import LhdSampling
